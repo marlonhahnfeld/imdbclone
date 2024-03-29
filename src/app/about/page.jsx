bb88432c3ebc32ca241d6701ead6b6d376954819
Marlon Hahnfeld <130953452+marlonhahnfeld@users.noreply.github.com>
2024-03-29 21:57:27 +0100

    created the about page with routing to /about

---
+++ src/app/about/page.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className=" max-w-6xl mx-auto p-3 space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod,
+        urna ut lacinia lacinia, nunc nunc tincidunt nunc, id aliquet nisl nunc
+        id nunc. Nulla facilisi. Sed euismod, urna ut lacinia lacinia, nunc nunc
+        tincidunt nunc, id aliquet nisl nunc id nunc. Nulla facilisi. Sed
+        euismod, urna ut lacinia lacinia, nunc nunc tincidunt nunc, id aliquet
+        nisl nunc id nunc. Nulla facilisi. Sed euismod, urna ut lacinia lacinia,
+        nunc nunc tincidunt nunc, id aliquet nisl nunc id nunc. Nulla facilisi.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod,
+        urna ut lacinia lacinia, nunc nunc tincidunt nunc, id aliquet nisl nunc
+        id nunc. Nulla facilisi. Sed euismod, urna ut lacinia lacinia, nunc nunc
+        tincidunt nunc, id aliquet nisl nunc id nunc. Nulla facilisi. Sed
+        euismod, urna ut lacinia lacinia, nunc nunc tincidunt nunc, id aliquet
+        nisl nunc id nunc. Nulla facilisi. Sed euismod, urna ut lacinia lacinia,
+        nunc nunc tincidunt nunc, id aliquet nisl nunc id nunc. Nulla facilisi.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod,
+        urna ut lacinia lacinia, nunc nunc tincidunt nunc, id aliquet nisl nunc
+        id nunc. Nulla facilisi. Sed euismod, urna ut lacinia lacinia, nunc nunc
+        tincidunt nunc, id aliquet nisl nunc id nunc. Nulla facilisi. Sed
+        euismod, urna ut lacinia lacinia, nunc nunc tincidunt nunc, id aliquet
+        nisl nunc id nunc. Nulla facilisi. Sed euismod, urna ut lacinia lacinia,
+        nunc nunc tincidunt nunc, id aliquet nisl nunc id nunc. Nulla facilisi.
+      </p>
+    </div>
+  );
+}
