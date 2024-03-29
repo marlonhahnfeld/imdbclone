1a674a530201b19ce505f18a4b4f9ca0e461e1df
Marlon Hahnfeld <130953452+marlonhahnfeld@users.noreply.github.com>
2024-03-29 23:39:30 +0100

    add loading effect using next js
    next revalidate every 10000 sec

---
+++ src/app/loading.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function loading() {
+  return (
+    <div className="flex justify-center">
+      {" "}
+      <span className="loading loading-spinner loading-lg"></span>
+    </div>
+  );
+}
