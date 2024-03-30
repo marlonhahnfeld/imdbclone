420a3fa8513da6fc632e8f17f6ae6f7395b4dfc7
Marlon Hahnfeld <130953452+marlonhahnfeld@users.noreply.github.com>
2024-03-30 16:21:24 +0100

    navbaritem suspense added

--- src/components/NavbarItem.jsx
+++ src/components/NavbarItem.jsx
@@ -1,6 +1,6 @@
 "use client";
 import Link from "next/link";
-import React from "react";
+import React, { Suspense } from "react";
 import { useSearchParams } from "next/navigation";
 
 export default function NavbarItem({ title, param }) {
@@ -8,17 +8,19 @@ export default function NavbarItem({ title, param }) {
   const genre = SearchParams.get("genre");
 
   return (
-    <div>
-      <Link
-        className={`hover:text-amber-600 font-semibold ${
-          genre === param
-            ? "underline underline-offset-8 decoration-4 decoration-amber-400 rounded-lg"
-            : ""
-        }`}
-        href={"/?genre=" + param}
-      >
-        {title}
-      </Link>
-    </div>
+    <Suspense>
+      <div>
+        <Link
+          className={`hover:text-amber-600 font-semibold ${
+            genre === param
+              ? "underline underline-offset-8 decoration-4 decoration-amber-400 rounded-lg"
+              : ""
+          }`}
+          href={"/?genre=" + param}
+        >
+          {title}
+        </Link>
+      </div>
+    </Suspense>
   );
 }
