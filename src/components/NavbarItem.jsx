094a174246e5eb73f0e51ca8afeb35085076d02e
Marlon Hahnfeld <130953452+marlonhahnfeld@users.noreply.github.com>
2024-03-30 17:17:40 +0100

    suspend

--- src/components/NavbarItem.jsx
+++ src/components/NavbarItem.jsx
@@ -3,24 +3,30 @@ import Link from "next/link";
 import React, { Suspense } from "react";
 import { useSearchParams } from "next/navigation";
 
-export default function NavbarItem({ title, param }) {
+function LinkWithParams({ title, param }) {
   const SearchParams = useSearchParams();
   const genre = SearchParams.get("genre");
 
   return (
-    <Suspense>
-      <div>
-        <Link
-          className={`hover:text-amber-600 font-semibold ${
-            genre === param
-              ? "underline underline-offset-8 decoration-4 decoration-amber-400 rounded-lg"
-              : ""
-          }`}
-          href={"/?genre=" + param}
-        >
-          {title}
-        </Link>
-      </div>
+    <div>
+      <Link
+        className={`hover:text-amber-600 font-semibold ${
+          genre === param
+            ? "underline underline-offset-8 decoration-4 decoration-amber-400 rounded-lg"
+            : ""
+        }`}
+        href={"/?genre=" + param}
+      >
+        {title}
+      </Link>
+    </div>
+  );
+}
+
+export default function NavbarItem({ title, param }) {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <LinkWithParams title={title} param={param} />
     </Suspense>
   );
 }
